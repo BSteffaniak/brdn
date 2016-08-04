4c428309f3685dd30778312c8f1cc258e72d9dc3
Braden Steffaniak <BradenSteffaniak@gmail.com>
2016-08-03 21:15:08 -0500

    Remove unnecessary files

--- Error/Error.js
+++
@@ -1,6 +0,0 @@
-app.controller('Error', [ '$scope', function($scope) {
-	if (typeof $scope.params !== 'undefined') {
-		$scope.errorType = $scope.params.errorType;
-		$scope.errorMessage = $scope.params.errorMessage;
-	}
-}]);
