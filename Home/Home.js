8eab0859d3a4dd96bf9cac3b8c09aa80d81954a0
Braden Steffaniak <BradenSteffaniak@gmail.com>
2016-08-03 23:16:09 -0500

    Renamed journey to next.

--- Home/Home.js
+++ Home/Home.js
@@ -1,6 +1,5 @@
 app.controller('Home', [ '$scope', function($scope) {
-	d3.select("#journey").on("click", function () {
-		console.log("asdf", $(window).height())
+	d3.select("#next").on("click", function () {
 		$('div.view').animate({
           scrollTop: $(window).height()
         }, 1000);
