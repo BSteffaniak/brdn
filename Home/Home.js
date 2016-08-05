8520649ca519bd3640d779d2a4bb2d72f7112b79
Braden Steffaniak <BradenSteffaniak@gmail.com>
2016-08-05 17:10:50 -0500

    Added animation

--- Home/Home.js
+++ Home/Home.js
@@ -4,4 +4,20 @@ app.controller('Home', [ '$scope', function($scope) {
           scrollTop: $(window).height()
         }, 1000);
 	});
+	
+	var slides = d3.selectAll(".slide")[0];
+	
+	var view = d3.select("div.view");
+	
+	function refreshSlides() {
+		for (var i = 0; i < slides.length; i++) {
+			var distance = Math.abs(view.node().scrollTop - slides[i].offsetTop);
+			
+			d3.select(slides[i]).classed("active", distance < 100);
+		}
+	}
+	
+	view.on("scroll", refreshSlides);
+	
+	refreshSlides();
 }]);
