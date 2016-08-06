b80ba2f83d274a93659caae58a988fb77f8a8e84
Braden Steffaniak <BradenSteffaniak@gmail.com>
2016-08-06 15:36:44 -0500

    Updated boeing slide.

--- App.js
+++ App.js
@@ -529,10 +529,15 @@ app.controller('App', [ '$timeout', '$window', '$interpolate', '$filter', '$http
 				d3.select(slides[i]).classed("active", distance < 100);
 				d3.select(slides[i]).classed("visible", y < scrollBottom && y + height > scroll);
 				
-				$scope.slides[slides[i].id] = $scope.slides[slides[i].id] || {};
-				
-				var slide = $scope.slides[slides[i].id];
-				slide.screenY = y - scroll;
+				if (slides[i].id) {
+					$scope.slides[slides[i].id] = $scope.slides[slides[i].id] || {};
+					
+					var slide = $scope.slides[slides[i].id];
+					slide.screenY = y - scroll;
+					
+					if (slides[i].id=="boeing")
+					console.log(slides[i].id, slide.screenY);
+				}
 			}
 			
 			if (apply !== false) {
